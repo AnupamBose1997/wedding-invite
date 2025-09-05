e65e15913fbada959ab6bd8624a8879b8061db22
Anupam <anupam.bose21@gmail.com>
2025-09-05 19:19:15 +0100

    Add .vercelignore and update configurations to exclude sanity-studio
    - Created .vercelignore to specify files and directories to ignore during deployment.
    - Updated next.config.js to exclude the sanity-studio directory from the build process.
    - Modified tsconfig.json to exclude sanity-studio from TypeScript compilation.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['cdn.sanity.io'],
   },
+  // Exclude sanity-studio from build
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('sanity-studio')
+    }
+    return config
+  },
+  // Exclude sanity-studio directory from compilation
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./sanity-studio/**/*']
+    }
+  }
 }
 
 module.exports = nextConfig
